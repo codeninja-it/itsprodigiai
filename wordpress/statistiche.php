5c795e47fab90b46a7688753e57d60beb6e7a6f7
CodeNinja srls <67374481+codeninja-it@users.noreply.github.com>
2025-03-24 18:01:59 +0100

    nuova versione con save_post_statistiche

--- wordpress/statistiche.php
+++ wordpress/statistiche.php
@@ -43,7 +43,7 @@
 	add_shortcode("statistica", "registraStatistica");
 	
 	
-	add_action("add_meta_boxes", "attivaBox");
+	add_action("add_meta_boxes_statistica", "attivaBox");
 	function attivaBox(){
 		add_meta_box(
 						"boxStatistica", 
@@ -52,6 +52,7 @@
 						null,
 						"side"
 					);
+		add_meta_box( "boxModificaVisite", "Visitatori totali", "moduloVisitatori" );
 	}
 	
 	function datiStatistica(){
@@ -59,4 +60,31 @@
 		echo "<b>[statistica id=".$_GET["post"]."]</b>";
 	}
 	
+	function moduloVisitatori(){
+		echo "<p>GET: ".json_encode($_GET)."</p>";
+		echo "<p>POST: ".json_encode($_POST)."</p>";
+		$idPost = intval($_GET["post"]);
+		$visite = get_post_meta($idPost, "visite", true);
+		echo "<p>
+					<label for='nvisite'>Visite</label>
+					<input type='number' value='".$visite."' name='nvisite' id='nvisite'>
+				</p>";
+	}
+	
+	add_action("save_post_statistica", "salvaStatistica");
+	
+	function salvaStatistica(){
+		// costruisco un array con dentro sia get che post
+		$stato = array(
+					"get" => $_GET,
+					"post" => $_POST
+					);
+		// lo convert in json
+		$buffer = json_encode($stato, JSON_PRETTY_PRINT);
+		// lo salvo su disco per vedere cosa vede il mio server
+		// quando gli chiedo di salvare un post
+		file_put_contents(__DIR__ . "/registro_statistiche.json", $buffer);
+		
+		update_post_meta( $_POST["ID"], "visite", $_POST["nvisite"]);
+	}
 	
