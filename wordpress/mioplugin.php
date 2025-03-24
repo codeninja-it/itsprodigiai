83e3ade70df7d8365034a911d34aa70bdf37a306
CodeNinja srls <67374481+codeninja-it@users.noreply.github.com>
2025-03-24 11:12:36 +0100

    ultima versione del plugin shortcodes

--- wordpress/mioplugin.php
+++ wordpress/mioplugin.php
@@ -30,6 +30,35 @@ function tabellina(){
 	// altrimenti leggo i parametri get base e ripetizioni
 	// e con questi stampo quella tabellina
 	// es.: ?base=2&ripetizioni=10 => <p>2 x 1 = 2</p>[..]<p>2 X 10 = 20</p>
+	$buffer = "";
+	
+	if(isset($_GET["base"]) && isset($_GET["ripetizioni"])){
+		$base = $_GET["base"];
+		$ripetizioni = $_GET["ripetizioni"];
+		$buffer .= "<h3>Tabellina del ".$base."</h3>";
+		for($i=0; $i < $ripetizioni ; $i++){
+			$risultato = $base * $i;
+			$buffer .= "<p>".$base." X ".$i." = ".$risultato."</p>";
+		}
+	} else {
+		$buffer = "<form>
+						<p><input name='base' placeholder='base della tabellina' type='number' /></p>
+						<p><input name='ripetizioni' placeholder='numero di righe' type='number' /></p>
+						<button>Genera</button>
+					</form>";
+	}
+	
+	return $buffer;
 }
 
 add_shortcode("miaTabellina", "tabellina");
+
+
+function analisi($attributi, $contenuto, $nometag){
+	$testoDaSpedire = "<h6>TAG ".$nometag."</h6>";
+	$testoDaSpedire .= "<p>attributi: " . json_encode($attributi, JSON_NUMERIC_CHECK) . "</p>";
+	$testoDaSpedire .= "<p>contenuto: " . $contenuto . "</p>";
+	return $testoDaSpedire;
+}
+
+add_shortcode("miaAnalisi", "analisi");
